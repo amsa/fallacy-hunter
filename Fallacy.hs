66ac2cd885da5d8b9755bae37e98ca771346a4e1
amin <amin.w3dev@gmail.com>
2014-12-11 18:58:11 -0500

    Added recursive data structure to represent triple tree

--- Fallacy.hs
+++ Fallacy.hs
@@ -1,4 +1,18 @@
+module Fallacy where
+
 import Data.Logic.Propositional
+
+
+type Subj = String
+type Pred = String
+type Obj = String
+
+data TripleTree = Triple Subj Pred Obj
+          | Neg TripleTree
+          | Conj TripleTree TripleTree
+          | Disj TripleTree TripleTree
+          | Cond TripleTree TripleTree
+          deriving (Eq, Show)
 
 var = Variable . Var
 neg = Negation
@@ -11,4 +25,4 @@ p = var 'p'
 q = var 'q'
 r = var 'r'
 
-fallacy1 = isTautology ((p `disj` q) `conj` p) `cond` (neg q)
+fallacy1 = isTautology $ ((p `disj` q) `conj` p) `cond` (neg q)
