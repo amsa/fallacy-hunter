e50df50e42ba2e31f823872685999c175c6080fc
amin <amin.w3dev@gmail.com>
2014-12-11 17:58:49 -0500

    added formula for the first fallacy

---
+++ Fallacy.hs
@@ -0,0 +1,14 @@
+import Data.Logic.Propositional
+
+var = Variable . Var
+neg = Negation
+conj = Conjunction
+disj = Disjunction
+cond = Conditional
+iff = Biconditional
+
+p = var 'p'
+q = var 'q'
+r = var 'r'
+
+fallacy1 = isTautology ((p `disj` q) `conj` p) `cond` (neg q)
