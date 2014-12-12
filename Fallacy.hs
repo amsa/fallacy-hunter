646e6432e5269a1d94dc210553fa2b5e694cb785
Georg Konwisser <gekonwi@gmail.com>
2014-12-11 19:14:52 -0500

    #6: implemented prototype of fallacy checking for 'Affirming a Disjunct'

--- Fallacy.hs
+++ Fallacy.hs
@@ -7,8 +7,26 @@ disj = Disjunction
 cond = Conditional
 iff = Biconditional
 
+a = var 'a'
 p = var 'p'
 q = var 'q'
 r = var 'r'
 
-fallacy1 = isTautology $ ((p `disj` q) `conj` p) `cond` (neg q)
+affirmingDisjunctLeft = ((p `disj` q) `conj` p)
+affirmingDisjunctRight = (neg q)
+affirmingDisjunct = affirmingDisjunctLeft `cond` affirmingDisjunctRight
+
+fol1Left = (((p `conj` a) `disj` (q `conj` a)) `conj` (p `conj` a))
+fol1Right = ((neg q) `conj` a)
+fol1 = fol1Left `cond` fol1Right
+
+
+fol2 = fol1Left `cond` a
+
+isFallacy :: Expr -> Bool
+isFallacy (Conditional left right) =
+	isTautology (left `cond` affirmingDisjunctLeft) &&
+	isTautology (right `cond` affirmingDisjunctRight)
+
+test1 = isFallacy fol1 -- expected: true
+test2 = isFallacy fol2 -- expected: false
